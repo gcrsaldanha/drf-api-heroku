72fed8c4ba50d915d558082b121c30bc47e3cba8
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-04 00:02:53 -0300

    Adiciona validações customizadas

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 
 from agenda.models import Agendamento
 
@@ -8,6 +9,19 @@ class AgendamentoSerializer(serializers.Serializer):
     nome_cliente = serializers.CharField(max_length=200)
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=20)
+
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        return value
+
+    def validate(self, attrs):
+        telefone_cliente = attrs.get("telefone_cliente", "")
+        email_cliente = attrs.get("email_cliente", "")
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+            raise serializers.ValidationError("E-mail brasileiro deve estar associado a um número do Brasil (+55)")
+        return attrs
 
     def create(self, validated_data):
         agendamento = Agendamento.objects.create(
