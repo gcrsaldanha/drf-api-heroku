94afc5f9b7da7b33b40f9b304c0847e59c3c016c
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-09 12:11:37 -0300

    Utiliza view genericas

--- agenda/views.py
+++ agenda/views.py
@@ -13,41 +13,14 @@ from agenda.serializers import AgendamentoSerializer
 from agenda.utils import get_horarios_disponiveis
 
 
-class AgendamentoList(
-    mixins.ListModelMixin,  # adicionar mixin de listagem
-    mixins.CreateModelMixin,  # adicionar mixin de criacao
-    generics.GenericAPIView,  # classe generica
-):
+class AgendamentoList(generics.ListCreateAPIView):  # /api/agendamentos/
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class AgendamentoDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class AgendamentoDetail(generics.RetrieveUpdateDestroyAPIView):  # /api/agendamentos/<pk>/
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-       
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
 @api_view(http_method_names=["GET"])
