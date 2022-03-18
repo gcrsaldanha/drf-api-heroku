24a16c6af56e1dcd573507ae1e4f7a5d8b5c7ac5
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-18 13:41:40 -0300

    Adiciona view de healthcheck

--- agenda/views.py
+++ agenda/views.py
@@ -83,3 +83,8 @@ def get_horarios(request):
 
     horarios_disponiveis = sorted(list(get_horarios_disponiveis(data)))
     return Response(horarios_disponiveis)
+
+
+@api_view(http_method_names=["GET"])
+def healthcheck(request):
+    return Response({"status": "OK"}, status=200)
