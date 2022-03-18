f4c6923c5e2c1e1bac0d6779917ff2293ed82182
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-18 15:16:58 -0300

    Corrige merge

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
-from agenda.views import AgendamentoDetail, AgendamentoList, get_horarios, get_relatorio_prestadores, index
-=======
 from agenda.views import AgendamentoDetail, AgendamentoList, get_horarios, get_relatorio_prestadores, healthcheck
->>>>>>> @{-1}
 
 urlpatterns = [
     path('agendamentos/', AgendamentoList.as_view()),
