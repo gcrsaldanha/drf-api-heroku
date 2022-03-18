13165dba06be8ea0fdc217baeb7d3565340f2aa3
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-18 13:57:42 -0300

    Introduz bug sem querer

--- agenda/views.py
+++ agenda/views.py
@@ -87,4 +87,5 @@ def get_horarios(request):
 
 @api_view(http_method_names=["GET"])
 def healthcheck(request):
+    raise ValueError("bug introduzido!")
     return Response({"status": "OK"}, status=200)
