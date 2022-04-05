1992ed3504bfc14d1e59ec774fe66d060f62452c
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-04-04 21:38:36 -0300

    Código que tá bonito

--- agenda/views.py
+++ agenda/views.py
@@ -58,7 +58,9 @@ class AgendamentoDetail(
 ):  # /api/agendamentos/<pk>/
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
-    permission_classes = [IsOwner]
+    permission_classes = [
+        IsOwner,
+    ]
 
     def perform_destroy(self, instance):
         instance.cancelado = True
